353893bded3b3ed5aac9cad940f3e19d559e2a48
MoartMedia <alex789189@gmail.com>
2021-06-25 20:26:51 +0900

    feat: add Command VoiceStatus

---
+++ Commands/music/status.js
@@ -0,0 +1,14 @@
+module.exports = {
+    name: '음성상태',
+    execute: async ({msg,bot,args}) => {
+        if(!bot._players.has(msg.guild.id)) return msg.reply('현재 플레이어가 실행 중이 아닙니다.');
+        const player = bot._players.get(msg.guild.id);
+        const ping = player.connection?.ping;
+        const pingUDP = ping?.udp||"?";
+        const pingWS = ping?.ws||"?"
+        return msg.reply(
+`음성 보이스: ${pingUDP}ms
+음성 웹소켓: ${pingWS}ms`
+)
+    }
+}
