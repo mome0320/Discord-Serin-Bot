a2d6b81f2dc3697d0fecdcf599e8a03fd148a250
MoartMedia <alex789189@gmail.com>
2021-06-29 16:19:37 +0900

    feat: cooldown other user click on cancel button

--- ButtonCommands/Cancel.js
+++ ButtonCommands/Cancel.js
@@ -1,6 +1,18 @@
 module.exports = {
   name: "취소",
-  execute: async ({ interaction, executor }) => {
+  execute: async ({ msg, interaction, executor }) => {
+    if (!msg.mentions.has(executor.id)) {
+      const ONE_MINUTE_PER_MILISECONDS = 60e3;
+      const afterTime = Date.now() - msg.createdTimestamp;
+      if (afterTime < ONE_MINUTE_PER_MILISECONDS) {
+        return interaction.reply({
+          content: `명령어 실행자가 아닌 사람은 ${
+            (ONE_MINUTE_PER_MILISECONDS - afterTime) / 1e3
+          }초 후에 취소할 수 있습니다.`,
+          ephemeral: true,
+        });
+      }
+    }
     interaction.update({
       content: `💽  ${executor}님이 취소 하셨습니다.`,
       components: [],
