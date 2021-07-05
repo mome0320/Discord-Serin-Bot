6605d6d9de505a66f1b832c6d17a5817a6d37228
MoartMedia <alex789189@gmail.com>
2021-07-05 12:19:38 +0900

    fix: remove test file

--- Object/test.js
+++
@@ -1,9 +0,0 @@
-const tetris = require("./Tetris");
-const newGame = new tetris(19, 9);
-const interval = setInterval(() => {
-  if (newGame.nowPiece == null) clearInterval(interval);
-  newGame.queue.rotate = 1;
-  newGame.queue.x = 1;
-  newGame.update();
-  console.table(newGame.viewGridHuman);
-}, 500);
